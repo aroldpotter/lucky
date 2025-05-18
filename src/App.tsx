fd45531a7dd4e1d6765c78a213c131ce7db31fbd
aroldpotter <luckymy1004@gmail.com>
2025-05-19 03:42:35 +0430

    again build

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ const App: React.FC = () => {
         ) : (
           <>
             <Welcome />
-            <BrowserRouter>
+            <BrowserRouter basename='/lucky'>
               <AnimatedRoutes />
             </BrowserRouter>
           </>
